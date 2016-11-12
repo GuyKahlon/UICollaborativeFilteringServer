cf632ec5c55169179dbe213e8e289157b690d880
Guy Kahlon <GuyKahlon@gmail.com>
2016-11-12 12:23:07 +0200

    Init

---
+++ Sources/App/main.swift
@@ -0,0 +1,17 @@
+import Vapor
+
+let drop = Droplet()
+
+drop.get("/") { request in
+  return "Hello World!"
+}
+
+drop.get("/name",":name") { request in
+  if let name = request.parameters["name"]?.string {
+    return "Hello \(name)!"
+  }
+  return "Error retrieving parameters."
+}
+
+
+drop.run()
