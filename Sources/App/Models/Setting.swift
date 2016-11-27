563506cc691774f80f0d05a43d8c153e149dc203
Guy Kahlon <GuyKahlon@gmail.com>
2016-11-27 17:17:47 +0200

    Update Main. Create new Setting model.

---
+++ Sources/App/Models/Setting.swift
@@ -0,0 +1,70 @@
+//
+//  Setting.swift
+//  ToDoServer
+//
+//  Created by Guy Kahlon on 11/27/16.
+//
+//
+
+import Vapor
+
+final class Setting: Model {
+  
+  var id: Node?
+  var exists: Bool = false
+  
+  
+  var textSize: String
+  var textStyle: String
+  var mode: String
+  var orientation: String
+  
+  init() {
+    self.id = nil
+    self.textSize = "textSize"
+    self.textStyle = "textStyle"
+    self.mode = "mode"
+    self.orientation = "orientation"
+  }
+
+  init(textSize: TextSize, textStyle: TextStyle, mode: Mode, orientation: Orientation) {
+    self.id = nil
+    self.textSize = "textSize"
+    self.textStyle = "textStyle"
+    self.mode = "mode"
+    self.orientation = "orientation"
+  }
+
+  init(node: Node, in context: Context) throws {
+    id = try node.extract("id")
+    textSize = try node.extract("textSize")
+    textStyle = try node.extract("textStyle")
+    mode = try node.extract("mode")
+    orientation = try node.extract("orientation")
+  }
+  
+  public func makeNode(context: Context) throws -> Node {
+    return try Node(node: [
+        "id": id,
+        "textSize": textSize,
+        "textStyle": textStyle,
+        "mode": mode,
+        "orientation": orientation,
+      ])
+  }
+  
+  static func prepare(_ database: Database) throws {
+    try database.create("settings") { users in
+      users.id()
+      users.string("textSize")
+      users.string("textStyle")
+      users.string("mode")
+      users.string("orientation")
+    }
+  }
+  
+  static func revert(_ database: Database) throws {
+    try database.delete("settings")
+  }
+}
+
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,38 +3,19 @@ import VaporPostgreSQL
 
 
 let drop = Droplet(
-  preparations: [Acronym.self],
+  preparations: [Setting.self],
   providers: [VaporPostgreSQL.Provider.self]
 )
-
-//drop.get("/") { request in
-//  return "Hello World!"
-//}
-
-drop.get("/name",":name") { request in
-  if let name = request.parameters["name"]?.string {
-    return "Hello \(name)!!!"
-  }
-  return "Error retrieving parameters."
-}
 
 drop.get { request in
   
   return try JSON(node: [
-    
-      "message" : "Hello, Vapor JSON !!!"
-  ])
+    "message" : "Hello, Vapor JSON !!!"
+    ])
 }
 
-drop.get("beers", Int.self) { request, beers in
-  return "Pass it around \(beers) :)"
-}
-
-
-
-
+//Return the Data Base version
 drop.get("version") { request in
-  
   if let db = drop.database?.driver as? PostgreSQLDriver {
     let version = try db.raw("SELECT version()")
     return try JSON(node: version)
@@ -43,23 +24,134 @@ drop.get("version") { request in
   }
 }
 
-//DD
+//Create preferences for user withput saving
 
 drop.get("model") { request in
-  let ancronym = Acronym(short: "AFK", long: "Away From Keyboard")
-  return try ancronym.makeJSON()
+  //  let preferences = UIPreference(textSize: .small,
+  //                                 textStyle: .regular,
+  //                                 mode: .light,
+  //                                 orientation: .portrait)
+  //
+  //  return try preferences.makeJSON()
+  
+  let s = Setting()
+  return try s.makeJSON()
 }
-
 
 drop.get("test") { request in
-  var ancronym = Acronym(short: "AFK", long: "Away From Keyboard")
-  try ancronym.save()
   
-  return try JSON(node:[
-    Acronym.all().makeNode()
-  ])
+  //  var preferences = UIPreference(
+  //    textSize: .small,
+  //    textStyle: .regular,
+  //    mode: .light,
+  //    orientation: .portrait
+  //  )
+  //
+  //  try preferences.save()
+  //
+  //  return try JSON(node:UIPreference.all().makeNode())
+  
+  var s = Setting()
+  try s.save()
+  
+  return try JSON(node: Setting.all().makeNode())
 }
 
+
+//Return all the objects from DB
+
+drop.get("all") { request in
+  return try JSON(node: Setting.all().makeNode())
+}
+
+//let preferencesController = PreferencesController()
+//drop.resource("preferences", preferencesController)
+
+
+
+
+//drop.get("/name",":name") { request in
+//  if let name = request.parameters["name"]?.string {
+//    return "Hello \(name)!!!"
+//  }
+//  return "Error retrieving parameters."
+//}
+
+
+
+//drop.get("beers", Int.self) { request, beers in
+//  return "Pass it around \(beers) :)"
+//}
+//
+//let basic = BasicController()
+//basic.addRoutes(drop: drop)
+
+//DD
+
+
+
+
+
+
+//Create Test Preferences Object
+
+
+
+// MARK: - Data Base Operations
+
+
+
+////Create a new preferences object and save at the DB
+//drop.post("new") { request in
+//  
+//  var preferences = try UIPreference(node: request.json)
+//  try preferences.save()
+//
+//  return preferences
+//}
+//
+//
+////Return the first object in the DB
+//drop.get("first") { request in
+//  return try JSON(node: UIPreference.query().first()?.makeNode())
+//}
+//
+////Return all the preferences with text size small 
+//drop.get("small") { request in
+//  
+//  let node = try UIPreference.query().filter("textSize", TextSize.small.rawValue).all().makeNode()
+//  return try JSON(node: node)
+//}
+//
+////Return all the preference with non small text size
+//
+//drop.get("non-small") { request in
+//  
+//  let node = try UIPreference.query().filter("textSize", .notEquals, TextSize.small.rawValue).all().makeNode()
+//  return try JSON(node: node)
+//}
+//
+////Update the first
+////8080/update?=textSize=2
+//drop.get("update") { request in
+//  
+//  guard var first = try UIPreference.query().first(), let textSize = request.data["textSize"]?.int else {
+//    throw Abort.badRequest
+//  }
+//  
+//  first.textSize = "1"//TextSize(rawValue: textSize)!.rawValue
+//  try first.save()
+//  return first
+//}
+//
+////Delete
+//
+//drop.get("delete-afks") { request in
+//  
+//  let query = try UIPreference.query().filter("textSize", TextSize.small.rawValue)
+//  try query.delete()
+//  return try JSON(node: UIPreference.all().makeNode())
+//}
 
 // MARK: - Run
 drop.run()
